0fe6b5915a3f0cd76989723c80675e46290c73dd
psyco315 <parth2004batman@gmail.com>
2025-09-11 20:45:50 +0530

    Deploying

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000
 
 // Middlewares
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://booklog-client.vercel.app'],
+    origin: ['http://localhost:5173', 'https://optical-document-summary-server.vercel.app/'],
     credentials: true
 }));
 app.use(bodyParser.json());
